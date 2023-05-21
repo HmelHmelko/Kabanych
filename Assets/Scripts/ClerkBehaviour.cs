23765944f0704d6bbea6e94823ed7a35f15804e6
HmelHmelko <48437486+HmelHmelko@users.noreply.github.com>
2023-05-21 15:00:39 +0300

    Initial commit

---
+++ Assets/Scripts/ClerkBehaviour.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClerkBehaviour : MonoBehaviour
+{
+
+    Rigidbody2D rb2D;
+
+    [SerializeField]
+    private float movementSpeed;
+
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        //rb2D.velocity = new Vector2(movementSpeed * Time.fixedDeltaTime, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerReachZone")
+        {
+            movementSpeed *= -1;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Kabanych
+ 
